5705e841bcd6d295a2d90bf7bb1cef4bb9558eaa
daeshov <jdarahthomas@gmail.com>
2023-05-12 01:37:23 +0000

    taske files

--- ES6_basic/9-getFullBudget.js
+++ ES6_basic/9-getFullBudget.js
@@ -3,13 +3,13 @@ import getBudgetObject from './7-getBudgetObject';
 export default function getFullBudgetObject(income, gdp, capita) {
   const budget = getBudgetObject(income, gdp, capita);
   const fullBudget = {
-  ...budget,
-  getIncomeInDollars(income) {
-  return `$${income}`;
-  },
-  getIncomeInEuros(income) {
-  return `${income} euros`;
-  },
+    ...budget,
+    getIncomeInDollars(income) {
+    return `$${income}`;
+    },
+    getIncomeInEuros(income) {
+    return `${income} euros`;
+    },
   };
 
   return fullBudget;
