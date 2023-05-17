0fe00ce618585a176ca7c685f2145726c6ccaa48
daeshov <jdarahthomas@gmail.com>
2023-05-17 20:06:33 +0000

    task 0

---
+++ ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,11 @@
+function getListStudents() {
+  const obj = [
+    { id: '1', firstname: 'Guillaume', location: 'San Francisco' },
+    { id: '2', firstname: 'James', location: 'Columbia' },
+    { id: '5', firstname: 'Serena', location: 'San Francisco' },
+  ];
+
+  return obj;
+}
+
+export default getListStudents;
