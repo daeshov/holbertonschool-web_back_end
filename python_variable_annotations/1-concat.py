9ab5d198ea4f70a16962a1eb3901c55297afb3ab
daeshov <jdarahthomas@gmail.com>
2023-09-05 17:32:25 -0500

    task 1

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -5,4 +5,4 @@ a string str2 as arguments and returns a concatenated string"""
 
 def concat(str1: str, str2: str) -> str:
       """return a str"""
-    return str1 + str2
+      return str1 + str2
