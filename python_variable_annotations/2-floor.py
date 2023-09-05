857d2f3b45b33b5a59b58c81767f2e35e7e48fb7
daeshov <jdarahthomas@gmail.com>
2023-09-05 18:05:42 -0500

    task 2

--- python_variable_annotations/2-floor.py
+++ python_variable_annotations/2-floor.py
@@ -3,6 +3,6 @@
 """ a type-annotated function floor which takes a float 
 n as argument and returns the floor of the float"""
 
-def floor(n: float) -> float:
+def floor(n: int) -> float:
       """returns n"""
       return n
