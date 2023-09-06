972ae80a9e896fade68120dedc16fd3e9c6c64c2
daeshov <jdarahthomas@gmail.com>
2023-09-05 19:31:50 -0500

    task 8

---
+++ python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+"""type-annotated function to_kv that takes a string k
+and an int OR float v as arguments and returns a tuple. The first element of the tuple is the string k. 
+The second element is the square of the int/float v and should be annotated as a float"""
+from typing import Callable
+
+def make_multiplier(multiplier: float) ->  Callable[[float], float]:
+
+def multiplier_function(x: float) -> float:
+    return x *multiplier
+
+
+return multiplier_function
