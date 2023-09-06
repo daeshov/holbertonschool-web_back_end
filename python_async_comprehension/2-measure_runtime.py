2580f23a36da53c3241cf3447338793b075ed70e
daeshov <jdarahthomas@gmail.com>
2023-09-06 14:23:37 -0500

    task 2

---
+++ python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""takes no arguments"""
+import asyncio
+import random
+from time import perf_counter
+from typing import Generator, List 
+async_generator = __import__('1-async_comprehension').async_generator
+
+
+async def measure_runtime () -> List[float]:
+    """"""
+    start = perf_counter()
+
+    await asyncio.gather(
+    async_comprehension(),
+    async_comprehension(),
+    async_comprehension(),
+    async_comprehension())
+
+    end = perf_counter()
+    total_runtime = end - start
+    return total_runtime
