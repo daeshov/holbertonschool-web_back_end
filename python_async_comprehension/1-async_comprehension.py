11fdcd8cefda018b4b60b7cfa7abae9abfe511f8
daeshov <jdarahthomas@gmail.com>
2023-09-06 13:46:17 -0500

    task 1

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """takes no arguments"""
-from async_generator import async_generator
 import asyncio
 import random
 from typing import Generator, List 
+async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension() -> List[float]:
