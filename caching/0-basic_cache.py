e90ac054c86fd9bc586e32fcccb49ecd0ed4e32a
daeshov <jdarahthomas@gmail.com>
2023-09-06 16:00:50 -0500

    task 0

---
+++ caching/0-basic_cache.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+    """ task 0 """
+from base_caching import BaseCaching
+
+class BasicCache(BaseCaching):
+    """ creating class """
+def put(self, key, item):
+  if key is None or item is None:
+    return self.cache_data[key] = item
+
+def get(self, key):
+    """ returns value linked to key """
+  if key is None:
+    return None
+  return self.cache_data.get(key, None)
--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -6,7 +6,7 @@ from typing import List
 
 
 async def wait_random(max_delay: int = 10) -> float:
-        """returns """
+    """returns """
     delay = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
     return delay
