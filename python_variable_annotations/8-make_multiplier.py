37025a912b524bae4aade53f2afa065b1d083813
daeshov <jdarahthomas@gmail.com>
2023-09-05 19:36:53 -0500

    task 7

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -9,4 +9,5 @@ def make_multiplier(multiplier: float) ->  Callable[[float], float]:
 
   def multiplier_function(x: float) -> float:
       return x *multiplier
-return multiplier_function
+
+  return multiplier_function
