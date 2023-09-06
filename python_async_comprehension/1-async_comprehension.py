7cb849071b62bb006bf1b136b033ea13d945ad8d
daeshov <jdarahthomas@gmail.com>
2023-09-06 14:50:00 -0500

    task 1

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
-"""takes no arguments"""
+"""task 1: async """
+
+
 import asyncio
 import random
-from typing import Generator, List 
+from typing import Generator, List
 async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension() -> List[float]:
-    """"""
+    """random 10"""
     random_numbers = [i async for i in async_generator()]
     return random_numbers
