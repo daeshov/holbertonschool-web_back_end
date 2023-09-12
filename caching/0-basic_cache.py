9213ebc030238484bc2a20794cf531e847bb2e53
daeshov <jdarahthomas@gmail.com>
2023-09-12 14:31:40 -0500

    task 0

--- caching/0-basic_cache.py
+++ caching/0-basic_cache.py
@@ -9,7 +9,7 @@ class BasicCache(BaseCaching):
 
 def put(self, key, item):
     if key is None or item is None:
-        return self.cache_data[key] = item
+        self.cache_data[key] = item
 
 
 def get(self, key):
