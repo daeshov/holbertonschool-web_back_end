149faa7c0a029242b0e2b46984fd3ed3195e2cc4
daeshov <jdarahthomas@gmail.com>
2023-09-12 16:40:35 -0500

    task 0

--- caching/0-basic_cache.py
+++ caching/0-basic_cache.py
@@ -17,6 +17,6 @@ class BasicCache(BaseCaching):
 
     def get(self, key):
         """ returns item by key """
-        if key is not None in self.cache_data:
+        if key is not None:
             return self.cache_data.get[key]
             return None
