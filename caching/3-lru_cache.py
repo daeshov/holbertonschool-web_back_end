6b0a8be7af3f2a637289a448a07c86bbd3ccdc37
daeshov <jdarahthomas@gmail.com>
2023-09-12 18:59:19 -0500

    task 3

--- caching/3-lru_cache.py
+++ caching/3-lru_cache.py
@@ -20,11 +20,10 @@ class LRUCache(BaseCaching):
                 print(f"DISCARD: {least_used}")
             self.insertion_order.append(key)
             self.cache_data[key] = item
-            
 
     def get(self, key):
         """ returns item by key """
         if key is not None:
-            self.insertion_order.remove(key)            
+            self.insertion_order.remove(key)
             self.insertion_order.append(key)
             return self.cache_data.get(key, None)
