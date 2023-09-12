560b87fdf23f02d08b976430f3fda97df788c976
daeshov <jdarahthomas@gmail.com>
2023-09-12 18:26:13 -0500

    task 3

---
+++ caching/3-lru_cache.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+""" task 3 Create a class LRUCache that inherits
+from BaseCaching and is a caching system
+"""
+from base_caching import BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """ creating LRUCache class """
+    def __init__(self):
+        super().__init__()
+        self.insertion_order = []
+
+    def put(self, key, item):
+        """ adding an item to cache and implenting LRU """
+        if key is not None and item is not None:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                last_item = self.insertion_order.pop()
+                del self.cache_data[last_item]
+                print(f"DISCARD: {last_item}")
+            self.insertion_order.append(key)
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """ returns item by key """
+        if key is not None:
+            return self.cache_data.get(key, None)
