adb8ffa0e5124bb7184e2d2ac050b316f4a508e9
daeshov <jdarahthomas@gmail.com>
2023-09-12 16:12:55 -0500

    task 0 fix hopfully

--- caching/0-basic_cache.py
+++ caching/0-basic_cache.py
@@ -8,16 +8,15 @@ from base_caching import BaseCaching
 class BasicCache(BaseCaching):
     """ creating class """
     def __init__(self):
-      self.cache_data = {}
+        self.cache_data = {}
 
-def put(self, key, item):
-    """ adding an item to cache """
+    def put(self, key, item):
+        """ adding an item to cache """
     if key or item is None:
         self.cache_data[key] = item
 
-
-def get(self, key):
-    """ returns item by key """
+    def get(self, key):
+        """ returns item by key """
     if key is not None and key in self.cache_data:
         return self.cache_data.get[key]
     return None
