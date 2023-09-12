6888316e1564b1b979d18005c1392bd6f09a7019
daeshov <jdarahthomas@gmail.com>
2023-09-12 14:29:24 -0500

    task 0

--- caching/0-basic_cache.py
+++ caching/0-basic_cache.py
@@ -2,14 +2,18 @@
 """ task 0 """
 from base_caching import BaseCaching
 
+
 class BasicCache(BaseCaching):
     """ creating class """
+
+
 def put(self, key, item):
-  if key is None or item is None:
-    return self.cache_data[key] = item
+    if key is None or item is None:
+        return self.cache_data[key] = item
+
 
 def get(self, key):
     """ returns value linked to key """
-  if key is None:
-    return None
-  return self.cache_data.get(key, None)
+    if key is None:
+        return None
+    return self.cache_data.get(key, None)
