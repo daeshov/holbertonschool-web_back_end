c6a7011e5163a64d7c30bb60c2d7a8a9e5f2b5e7
daeshov <jdarahthomas@gmail.com>
2023-09-12 19:16:55 -0500

    task 0

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -3,5 +3,5 @@
 a function named index_range"""
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
-""" a function named index_range"""
-  return ((page - 1)* page_size, page * page_size)
+    """ a function named index_range"""
+    return ((page - 1)* page_size, page * page_size)
