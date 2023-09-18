454c2d0f11689befbfabb78d1a639f7296183199
daeshov <jdarahthomas@gmail.com>
2023-09-18 15:21:32 -0500

    pycode fix

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -7,9 +7,10 @@ import re
 from typing import List
 
 
-def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
-  """Returns: log message obfuscated"""
-  for field in fields:
+def filter_datum(fields: List[str], redaction: str, message: str,
+  separator: str) -> str:
+    """Returns: log message obfuscated"""
+    for field in fields:
     message = re.sub(f'{field}=(.*?){separator}',
       f'{field}={redaction}{separator}', message)
   return message
