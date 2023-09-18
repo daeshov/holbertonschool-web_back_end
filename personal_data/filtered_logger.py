a75446d1aa1bafae8fba84b3e6a6ae69df5adc21
daeshov <jdarahthomas@gmail.com>
2023-09-18 15:12:12 -0500

    separator fix

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -7,7 +7,7 @@ import re
 from typing import List
 
 
-def filter_datum(fields: List[str], redaction: str, message: str, separtor: str) -> str:
+def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
   """Returns: log message obfuscated"""
   for field in fields:
     message = re.sub(f'{field}=(.*?){separator}',
