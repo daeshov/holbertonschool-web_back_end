ae5b53196218ca75d9dfdc17c37d7820531f26f1
daeshov <jdarahthomas@gmail.com>
2023-09-19 19:24:38 -0500

    task 2

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -5,6 +5,7 @@ that returns the log message obfuscated
 import logging
 import re
 from typing import List
+PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'password')
 
 
 def filter_datum(fields: List[str], redaction: str, message: str,
@@ -37,3 +38,16 @@ class RedactingFormatter(logging.Formatter):
             message = filter_datum(
                 [field], self.REDACTION, message, self.SEPARATOR)
         return message
+
+
+def get_logger() -> logging.Logger:
+    """function that takes no arguments and
+    returns a logging.Logger object"""
+    logger = logging.getLogger("user_data")
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+    handler = logging.StreamHandler()
+    formatter = RedactingFormatter(List(PII_FIELDS))
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    return logger
