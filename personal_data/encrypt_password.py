cd3962fed97a5d13e977735733497aee94de84b4
daeshov <jdarahthomas@gmail.com>
2023-09-20 20:51:07 -0500

    task 6

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -29,6 +29,7 @@ def is_valid(hashed_password: bytes, password: str) -> bool:
       password (str): The plain-text password to validate.
 
   Returns:
-      bool: True if the provided password matches the hashed password, False otherwise.
+      bool: True if the provided password
+      matches the hashed password, False otherwise.
   """
     return bcrypt.Checkpw(password.encode('utf-8'), hashed_password)
