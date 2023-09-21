316223b20c267b12cebb472bb3b31d7378b397af
daeshov <jdarahthomas@gmail.com>
2023-09-20 20:37:30 -0500

    task 5

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import bcrypt
-"""function that expects one string
-    argument name password"""
+  """a module that encrypts passwords
+  """
 
 
 def hash_password(password: str) -> bytes:
