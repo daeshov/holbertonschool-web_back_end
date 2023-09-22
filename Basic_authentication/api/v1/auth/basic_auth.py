21d577c0871da4c9f301c493b03a6a824084c262
daeshov <jdarahthomas@gmail.com>
2023-09-22 15:05:26 -0500

    task 6/7

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,8 +1,16 @@
 #!/usr/bin/env python3
 """ BasicAuth """
+import base64
+from typing import TypeVar
 
 
-class Auth:
-
-    class BasicAuth(Auth):
-        pass
+class BasicAuth(Auth):
+    def extract_base64_authorization_header(
+            self, authorization_header: str) -> str:
+        if authorization_header is None:
+            return None
+        if not isinstance(authorization_header, str):
+            return None
+        if authorization_header[:6] != "Basic ":
+            return None
+    return authorization_header[6:]
