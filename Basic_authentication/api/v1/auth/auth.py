29dd720b7a55e801478264033e114f64118ba8ce
daeshov <jdarahthomas@gmail.com>
2023-09-21 20:13:35 -0500

    task 3

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -5,19 +5,30 @@ import request from flask
 
 
 class Auth():
+    """class to manage the API authentication.
+    """
 
-    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        if path is None:
-            return True
+       def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+            """ returns False - path
+            and excluded_paths
+            """
+               if path is None:
+                    return True
 
-        if excluded_paths is None or len(excluded_paths) == 0:
-            return True
+                if excluded_paths is None or len(excluded_paths) == 0:
+                    return True
 
-    def authorization_header(self, request=None) -> str:
+        def authorization_header(self, request=None) -> str:
+            """returns None - request
+            will be the Flask request object
+            """
 
-        if request is None:
-            return None
+               if request is None:
+                    return None
 
-    def current_user(self, request=None) -> TypeVar('User'):
-        if request is None:
-            return None
+        def current_user(self, request=None) -> TypeVar('User'):
+            """None - request will be the
+            Flask request object
+            """
+               if request is None:
+                    return None
