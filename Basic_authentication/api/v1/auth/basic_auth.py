80e168cf06920a496e60a5001046bb9684020954
daeshov <jdarahthomas@gmail.com>
2023-09-24 16:58:16 -0500

    task 10

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -51,3 +51,28 @@ class BasicAuth(Auth):
             return None, None
         user_data = decoded_base64_authorization_header.split(':')
         return f"{user_email}:{user_password}"
+
+    def user_object_from_credentials(
+            self,
+            user_email: str,
+            user_pwd: str) -> TypeVar('User'):
+        """ returns the User instance based on his email and password
+        """
+        if not isinstance(user_email, str):
+            return None
+        if not isinstance(user_pwd, str):
+            return None
+        try:
+            users = user.search({'email': user_email})
+
+            if not users:
+                return None
+
+                user = user[0]
+
+            if not user.is_valid_password(user_pwd):
+                return None
+
+        except Exception:
+            return None
+        return None
