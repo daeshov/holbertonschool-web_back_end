ee9a52ef3e4244481add00d6ffba987eaaa6275a
daeshov <jdarahthomas@gmail.com>
2023-09-26 17:03:59 -0500

    task 2 fix

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -12,15 +12,13 @@ class SessionAuth(Auth):
     user_id_by_session_id = {}
 
     def create_session(self, user_id: str = None) -> str:
-      """creates a Session ID for a user_id
-      """
-      if user_id is None or not isinstance(user_id, str):
-         return None
+        """creates a Session ID for a user_id
+        """
+        if user_id is None or not isinstance(user_id, str):
+            return None
 
-      session_id = str(uuid.uuid4())
+        session_id = str(uuid.uuid4())
 
-      self.user_id_by_session_id[session_id] = user_id
+        self.user_id_by_session_id[session_id] = user_id
 
-      return session_id
-
-
+        return session_id
