e54066ba89935bffd609be8e6bdbe1678524b290
daeshov <jdarahthomas@gmail.com>
2023-09-26 18:25:58 -0500

    task 7

--- Session_authentication/api/v1/views/session_auth.py
+++ Session_authentication/api/v1/views/session_auth.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""a new Flask view that handles all routes for 
+"""a new Flask view that handles all routes for
 the Session authentication
 """
 from flask import Flask, request, jsonify, abort
@@ -7,8 +7,12 @@ from api.v1.views import app_views
 from models.user import User
 from os import environ
 
+
 @app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
 def session_login():
+    """oute, retrieves email and password
+    parameters from the form data
+    """
     email = request.form.get('email')
     password = request.form.get('password')
 
@@ -24,12 +28,16 @@ def session_login():
     if not user.is_valid_password(password):
         return jsonify({"error": "wrong password"}), 401
 
-    # Create a Session ID
+        # Create a Session ID
     from api.v1.app import auth
     session_id = auth.create_session(user.id)
 
     # Set the cookie with the session ID
     response = jsonify(user.to_json())
-    response.set_cookie(environ.get('SESSION_NAME', '_my_session_id'), session_id)
+    response.set_cookie(
+        environ.get(
+            'SESSION_NAME',
+            '_my_session_id'),
+        session_id)
 
     return response, 200
