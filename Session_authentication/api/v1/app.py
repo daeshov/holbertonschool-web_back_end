9de3cca8889c1d7e64957387a1eb0ec781e61467
daeshov <jdarahthomas@gmail.com>
2023-09-26 16:06:15 -0500

    task 0

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -68,8 +68,8 @@ def before_request():
     if not auth.require_auth(
         request.path, ['/api/v1/status/',
                        '/api/v1/unauthorized/',
-                       '/api/v1/forbidden/'
-                        '/api/v1/auth_session/login/'
+                       '/api/v1/forbidden/',
+                       '/api/v1/auth_session/login/'
                        ]):
         return
 
