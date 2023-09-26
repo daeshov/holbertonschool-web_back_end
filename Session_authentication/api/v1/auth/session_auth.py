b46662694b42e033238f8499a8a7596212ec8023
daeshov <jdarahthomas@gmail.com>
2023-09-26 16:45:09 -0500

    task 1

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python3
+import uuid
+from api.v1.auth.auth import Auth
+
+
 """
 class SessionAuth that inherits from Auth
 """
 class SessionAuth(Auth):
+  """class to manage session authentication
+  """  
   pass
