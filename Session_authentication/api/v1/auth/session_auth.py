6d9db1b439254d7bff9d8a56b32b6a53f2dd7c11
daeshov <jdarahthomas@gmail.com>
2023-09-26 16:47:04 -0500

    pycode task 1

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -7,6 +7,6 @@ from api.v1.auth.auth import Auth
 
 
 class SessionAuth(Auth):
-  """class to manage session authentication
-  """  
-  pass
+    """class to manage session authentication
+    """
+    pass
