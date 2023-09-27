2b688c25e3bf5a224b5af2338873432487244134
daeshov <jdarahthomas@gmail.com>
2023-09-27 17:06:04 -0500

    task 2

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -44,11 +44,6 @@ class DB:
         """fuction to find user
         """
         try:
-            user = self._session.query(User).filter_by(**kwargs).first()
-            if user is not None:
-                return user
-        except TypeError as e:
-            raise InvalidRequestError from e
-        if user is None:
-            raise NoResultFound
-        return user
+            return self._session.query(User).filter_by(**kwargs).one()
+        except (NoResultFound, InvalidRequestError):
+            raise
