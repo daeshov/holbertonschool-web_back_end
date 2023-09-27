dcb798844c5b2e0e3b581b6d4f988ab6667295a1
daeshov <jdarahthomas@gmail.com>
2023-09-27 12:56:01 -0500

    task 1

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -29,17 +29,12 @@ class DB:
             DBSession = sessionmaker(bind=self._engine)
             self.__session = DBSession()
         return self.__session
-    
-    def add_user(self, email:str, hashed_password: str) -> User:
-      """method should save the user
-      to the database
-      """        
-      user = User(email=email, hashed_password=hashed_password)
-      self.__session.add(user)
 
-      try:
-          self.__session.commit()
-          return user
-      except Exception as e:
-          self.__session.rollback()
-          raise
+    def add_user(self, email: str, hashed_password: str) -> User:
+        """method should save the user
+        to the database
+        """
+        user = User(email=email, hashed_password=hashed_password)
+        self._session.add(user)
+        self._session.commit()
+        return user
