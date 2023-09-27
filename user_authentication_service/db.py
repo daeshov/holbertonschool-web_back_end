9f6500f71b08e777b48441aebe07a21d35d0e68c
daeshov <jdarahthomas@gmail.com>
2023-09-27 14:11:30 -0500

    task 2

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -5,6 +5,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
 
 from user import Base, User
 
@@ -34,7 +36,19 @@ class DB:
         """method should save the user
         to the database
         """
-        user = User(email=email, hashed_password=hashed_password)
-        self._session.add(user)
+        newuser = User(email=email, hashed_password=hashed_password)
+        self._session.add(newuser)
         self._session.commit()
-        return user
+        return newuser
+
+    def find_user_by(self, **kwargs):
+        """fuction to find user
+        """
+        try:
+            user = self._session.query(User).filter_by(**kwargs).first()
+            if user is not None:
+                return user
+        except TypeError as e:
+            raise InvalidRequestError from e
+        if user is None:
+            return user
