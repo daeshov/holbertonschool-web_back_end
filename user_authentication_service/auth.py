7a0df1ddb1b8d15ffe96abdde2d9ba7284966f48
daeshov <jdarahthomas@gmail.com>
2023-09-27 18:25:10 -0500

    task 4

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -7,14 +7,9 @@ from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy.orm.exc import NoResultFound
 
 
-def _hash_password(self, password: str) -> bytes:
+def _hash_password(password: str) -> bytes:
     """The returned bytes is a salted
     hash of the input password
     """
     salt = bcrypt.gensalt()
-
-    return new_func(password, salt)
-
-
-def new_func(password, salt):
     return bcrypt.hashpw(password.encode('utf-8'), salt)
