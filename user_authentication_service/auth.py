5e78c3c6618b7bcba74c79a82cbdbbc0efe5ec1c
daeshov <jdarahthomas@gmail.com>
2023-09-27 18:48:53 -0500

    task 5

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -13,3 +13,22 @@ def _hash_password(password: str) -> bytes:
     """
     salt = bcrypt.gensalt()
     return bcrypt.hashpw(password.encode('utf-8'), salt)
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """should take mandatory email and password string
+      arguments and return a User object
+      """
+        try:
+            self._db.find_user_by(email=email)
+        except NoResultFound:
+            return self._db.add_user(email, _hash_password(password))
+        else:
+            raise ValueError(f"User {email} already exists.")
