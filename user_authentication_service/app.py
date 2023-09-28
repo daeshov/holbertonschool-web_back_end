a4e76c8909f19a3e6bc07f222a6223ca1def3657
daeshov <jdarahthomas@gmail.com>
2023-09-27 21:31:59 -0500

    task 14

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -43,5 +43,25 @@ def login() -> str:  # sourcery skip: use-named-expression
     return response
 
 
+@app.route('/sessions', methods=['DELETE'], strict_slashes=False)
+def logout() -> str:
+    """Logout function to destroy the session"""
+    session_id = request.cookies.get('session_id')
+
+    if not session_id:
+        # If session ID is not found, respond with a 403 status
+        return make_response(jsonify({"message": "Forbidden"}), 403)
+
+    user = AUTH.get_user_from_session_id(session_id)
+
+    if user:
+        # If the user exists, destroy the session and redirect to GET /
+        AUTH.destroy_session(user.id)
+        return redirect('/')
+    else:
+        # If the user does not exist, respond with a 403 status
+        return make_response(jsonify({"message": "Forbidden"}), 403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
