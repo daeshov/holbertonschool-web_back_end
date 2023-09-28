805020bcfc21fb57c34cd4afbf96ef53bb672ba2
daeshov <jdarahthomas@gmail.com>
2023-09-27 20:02:33 -0500

    task 8

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -32,3 +32,14 @@ class Auth:
             return self._db.add_user(email, _hash_password(password))
         else:
             raise ValueError(f"User {email} already exists.")
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """method to check password
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            return bcrypt.checkpw(
+                password.encode('utf-8'),
+                user.hashed_password)
+        except NoResultFound:
+            return False
