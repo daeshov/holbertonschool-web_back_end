0c396965be6c2e022b3d0b6a069d49cec894ffb0
daeshov <jdarahthomas@gmail.com>
2023-09-27 20:40:54 -0500

    task 11

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -26,6 +26,22 @@ def users():
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
 
+@app.route('/session', methods=['POST'], strict_slashes=False)
+def login() -> str:
+  """login method
+  """  
+  email = request.form.get('email')
+  password = request.form.get('password')
+  valid_login = Auth.valid_login(email, password)
+  if valid_login:
+      session_id = AUTH.create_session(email)
+      msg = {"email": email, "message": "logged in"}
+      response = jsonify(msg)
+      response.set_cookie('session_id', session_id)
+      return response
+  else:
+      abort(401)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
