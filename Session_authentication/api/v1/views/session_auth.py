3eea595e0b3b3d8639b0d7e8c2b4ff74b72b950e
daeshov <jdarahthomas@gmail.com>
2023-10-01 17:08:55 -0500

    file setup

--- Session_authentication/api/v1/views/session_auth.py
+++ Session_authentication/api/v1/views/session_auth.py
@@ -26,10 +26,15 @@ def session_login():
     except Exception:
         return jsonify({"error": "no user found for this email"}), 404
 
+    if not users:
+        return jsonify({"error": "no user found for this email"}), 404
+
     user = users[0]
 
     if not user.is_valid_password(password):
         return jsonify({"error": "wrong password"}), 401
+    
+    
 
     # Create a Session ID
     from api.v1.app import auth
---
+++ Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""utils test module
+"""
+
+import unittest
+from utils import access_nested_map
+
+
+class TestAccessNestedMap(unittest.TestCase):
+
+  def test_access_nested_map()
