cab8a3a653aa722da543b28f8f1e31e143e7c760
daeshov <jdarahthomas@gmail.com>
2023-10-02 17:39:34 -0500

    task 6

--- Unittests_and_integration_tests/test_client.py
+++ Unittests_and_integration_tests/test_client.py
@@ -32,13 +32,14 @@ class TestGithubOrgClient(unittest.TestCase):
         """
         with patch(
             'client.GithubOrgClient.org', new_callable=PropertyMock
-                ) as gc:
+        ) as gc:
             gc.return_value = {'repos_url': 'test.io'}
             org_client = client
             org_client = org_client.GithubOrgClient('test_org')
             self.assertEqual(
                 org_client.org['repos_url'], org_client._public_repos_url
             )
+
     @patch("client.get_json", return_value={'key': '12345'})
     def test_public_repos(self, get_json_mock):
         """
@@ -46,7 +47,7 @@ class TestGithubOrgClient(unittest.TestCase):
         get_json_mock()
         with patch(
             'client.GithubOrgClient._public_repos_url',
-                new_callable=PropertyMock ) as msk:
+                new_callable=PropertyMock) as msk:
             msk.return_value = {'key': '12345'}
             test_obj = client
             test_obj = test_obj.GithubOrgClient('another_test')
@@ -68,6 +69,7 @@ class TestGithubOrgClient(unittest.TestCase):
             client.GithubOrgClient.has_license(dict_license, key_license),
             expc_result
         )
+
 
 if __name__ == '__main__':
     unittest.main()
