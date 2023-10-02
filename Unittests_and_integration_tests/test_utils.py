b1b1917805cc3729bc3c369329df51164bdb1322
daeshov <jdarahthomas@gmail.com>
2023-10-01 19:00:29 -0500

    task 1

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -20,5 +20,19 @@ class TestAccessNestedMap(unittest.TestCase):
         """
         self.assertEqual(access_nested_map(nested_map, path), map)
 
+    @parameterized.expand([
+       ({}, ("a,")),
+       ({"a": 1}, ("a", "b"))
+    ])
+
+    def test_access_nested_map_exception(self, nested_map, path):
+      """to test that a KeyError is raised for
+      the following inputs
+      """
+      with self.assertRaises(KeyError):
+          access_nested_map(nested_map, path)
+
+    
+
 if __name__ == '__main__':
     unittest.main()
