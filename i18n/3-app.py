8c22f0cc191ff356f4670d9b1b19536b9912553c
daeshov <jdarahthomas@gmail.com>
2023-10-03 15:23:10 -0500

    task 3

--- i18n/3-app.py
+++ i18n/3-app.py
@@ -41,7 +41,9 @@ def get_locale():
 def index():
     """reload page
     """
-    return render_template('3-index.html')
+    home_title = _("home_title")
+    home_header = _("home_header")
+    return render_template('3-index.html', home_title=home_title, home_header=home_header)
 
 
 if __name__ == '__main__':
