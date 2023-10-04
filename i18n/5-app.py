0144f363c6f57075c63cb41ae549f6a15d474bba
daeshov <jdarahthomas@gmail.com>
2023-10-04 16:09:46 -0500

    task 5

--- i18n/5-app.py
+++ i18n/5-app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """get_locale function with the babel.localeselector decorator."""
 from flask import Flask, render_template, request, g
-from flask_babel import Babel
+from flask_babel import Babel, gettext
 
 
 app = Flask(__name__, template_folder='templates')
@@ -54,7 +54,7 @@ def get_locale():
 @app.route('/', methods=["GET"])
 def message():
     """Reload page with translations."""
-    return render_template('4-index.html')
+    return render_template('5-index.html')
 
 
 if __name__ == "__main__":
