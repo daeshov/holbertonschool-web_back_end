3af7978d7bc5cb78f6e5140fe1ee3880abec1f1e
daeshov <jdarahthomas@gmail.com>
2023-10-04 15:13:32 -0500

    task 4

--- i18n/4-app.py
+++ i18n/4-app.py
@@ -32,7 +32,7 @@ def get_locale():
 @app.route('/', methods=["GET"])
 def message():
     """Reload page with translations."""
-    return render_template('3-index.html')
+    return render_template('4-index.html')
 
 
 if __name__ == "__main__":
