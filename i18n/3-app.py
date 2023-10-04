e7d6cd6342278b19ae92a07bedef5ee29588cb5d
daeshov <jdarahthomas@gmail.com>
2023-10-04 15:03:43 -0500

    task 3

--- i18n/3-app.py
+++ i18n/3-app.py
@@ -32,13 +32,7 @@ def get_locale():
 @app.route('/', methods=["GET"])
 def message():
     """Reload page with translations."""
-    # gettext to link translations string
-    home_title = gettext('home_title')
-    home_header = gettext('home_header')
-    return render_template(
-        '3-index.html',
-        home_title=home_title,
-        home_header=home_header)
+    return render_template('3-index.html')
 
 
 if __name__ == "__main__":
