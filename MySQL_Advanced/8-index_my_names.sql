45b1daeb5dcb1b86cb0c75dd5ff33edaaf5ee128
root <jdarahthomas@gmail.com>
2023-10-16 21:52:31 +0000

    task 8

--- MySQL_Advanced/8-index_my_names.sql
+++ MySQL_Advanced/8-index_my_names.sql
@@ -1,22 +1,2 @@
--- Calculate the average score for the user
-DELIMITER $$
-
-CREATE PROCEDURE ComputeAverageScoreForUser(
-    IN p_user_id INT
-)
-BEGIN
-    DECLARE avg_score DECIMAL(10, 2);
-    
-    -- Calculate the average score for the user
-    SELECT AVG(score) INTO avg_score
-    FROM corrections
-    WHERE user_id = p_user_id;
-    
-    -- Update the user's average score in the users table
-    UPDATE users
-    SET average_score = avg_score
-    WHERE id = p_user_id;
-    
-END $$
-
-DELIMITER ;
+-- creates an index idx_name_first on the table names and the first letter of name.
+CREATE INDEX `idx_name_first` ON names (name(1));
