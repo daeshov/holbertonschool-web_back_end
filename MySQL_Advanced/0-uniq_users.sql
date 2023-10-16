b0f22ca4744da9e1141e7312e657770c36924f0e
root <jdarahthomas@gmail.com>
2023-10-16 00:06:54 +0000

    task 0 fix

--- MySQL_Advanced/0-uniq_users.sql
+++ MySQL_Advanced/0-uniq_users.sql
@@ -1,8 +1,6 @@
 -- Check if the table already exists, and if it does, do nothing
-IF NOT EXISTS (SELECT 1 FROM information_schema.tables WHERE table_name = 'users') THEN
-    CREATE TABLE users (
+    CREATE TABLE IF NOT exists users (
         id serial PRIMARY KEY,
         email VARCHAR(255) NOT NULL UNIQUE,
         name VARCHAR(255)
     );
-END IF;
