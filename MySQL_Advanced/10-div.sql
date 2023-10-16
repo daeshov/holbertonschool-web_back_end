f2e249815c454abd13b449f47a4bf714007d78b4
root <jdarahthomas@gmail.com>
2023-10-16 22:14:37 +0000

    task 10

--- MySQL_Advanced/10-div.sql
+++ MySQL_Advanced/10-div.sql
@@ -1,6 +1,5 @@
 --Write a SQL script that creates a function SafeDiv that divides (and returns) the first by the second number
 DELIMITER //
-
 CREATE FUNCTION SafeDiv(a INT, b INT)
 RETURNS FLOAT
 BEGIN
