f226ff6d8730617ac4c1cd25801173b7224c8105
root <jdarahthomas@gmail.com>
2023-10-16 21:20:04 +0000

    task 5

--- MySQL_Advanced/5-valid_email.sql
+++ MySQL_Advanced/5-valid_email.sql
@@ -1,7 +1,9 @@
-DELIMITER $$
 -- Decrease the item quantity for the item associated with the new order
+DELIMITER $$
 CREATE TRIGGER decrease_item_quantity
 AFTER INSERT ON orders
 FOR EACH ROW UPDATE items
 SET quantity = quantity - NEW.number
 WHERE items.name = NEW.item_name;
+END $$
+DELIMITER ;
