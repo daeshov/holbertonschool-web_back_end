2920f778f3e169586a12c56044bca9668640d7fe
root <jdarahthomas@gmail.com>
2023-10-23 20:59:54 +0000

    task 4

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -1,8 +1,10 @@
 const http = require('http');
 
+
 const app = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello Holberton School!\n');
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello Holberton School!');
 });
 
 app.listen(1245);
