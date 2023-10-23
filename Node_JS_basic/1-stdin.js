c56263e3151ba156df05c24f24a48e079d9fa6bc
root <jdarahthomas@gmail.com>
2023-10-23 19:04:28 +0000

    task 1 newline fix

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -8,7 +8,8 @@ const rl = readline.createInterface({
 console.log('Welcome to Holberton School, what is your name?');
 
 rl.question('', (name) => {
+  name = name.trim();
   console.log(`Your name is: ${name}`);
-  console.log('This important software is now closing\n');
+  console.log('This important software is now closing');
   rl.close();
 });
