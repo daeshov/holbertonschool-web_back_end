1a5f3265f50736d8f59399ba2b666aac484cd3da
root <jdarahthomas@gmail.com>
2023-10-26 20:43:02 +0000

    task 7

--- unittests_in_js/7-skip.test.js
+++ unittests_in_js/7-skip.test.js
@@ -5,11 +5,11 @@ describe('Testing numbers', () => {
     expect(1 === 1).to.be.true;
   });
 
-  it.skip('2 is equal to 2', () => {
+  it('2 is equal to 2', () => {
     expect(2 === 2).to.be.true;
   });
 
-  it('1 is equal to 3', () => {
+  it.skip('1 is equal to 3', () => {
     expect(1 === 3).to.be.true;
   });
 
