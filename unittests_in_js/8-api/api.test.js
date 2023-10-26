72727191c4e542bcb115d37bdcdeb2eb4180e934
root <jdarahthomas@gmail.com>
2023-10-26 21:50:31 +0000

    task 8

--- unittests_in_js/8-api/api.test.js
+++ unittests_in_js/8-api/api.test.js
@@ -1,10 +1,9 @@
 const request = require('request');
 const chai = require('chai')
-const { expect } = require('chai');
 
 describe('API test', () => {
   it('respond with 200 code and body', (done) => {
-    request('http://localhost:7865', 'GET', (error, res, body) => {
+    request('http://localhost:7865', 'GET /', (error, res, body) => {
       if (error) throw error;
       expect(res.statusCode).to.equal(200);
       expect(body).to.equal('Welcome to the payment system');
