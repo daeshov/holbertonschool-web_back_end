dfe038537c47622b30cbb2a320eef0108db62f7e
root <jdarahthomas@gmail.com>
2023-10-26 20:36:09 +0000

    task 6 test

--- unittests_in_js/6-payment_token.test.js
+++ unittests_in_js/6-payment_token.test.js
@@ -1,26 +1,15 @@
-const chai = require('chai');
-const expect = chai.expect;
+const { expect } = require('chai');
 
-// Import the getPaymentTokenFromAPI function from your module
-const getPaymentTokenFromAPI = require('./your-payment-module'); // Replace with the actual import path
+const getPaymentTokenFromAPI = require('./6-payment_token');
 
 describe('getPaymentTokenFromAPI', () => {
-  it('should return a payment token when called with true', (done) => {
-    // Call the async function getPaymentTokenFromAPI with true
+  it('promise response from API should resolve', (done) => {
     getPaymentTokenFromAPI(true)
       .then((result) => {
-        // Assert that the result is not null or undefined
-        expect(result).to.not.be.null;
-        expect(result).to.not.be.undefined;
-        // Add any additional assertions you need based on the expected result
-        // For example, you could check the structure or properties of the result.
-        // You might also check if it's a valid payment token.
-
-        // Call the done callback to signal the completion of the async test
+        expect(result).to.eql({ data: 'Successful response from the API' });
         done();
       })
       .catch((error) => {
-        // Handle any errors that occur during the async operation
         done(error);
       });
   });
