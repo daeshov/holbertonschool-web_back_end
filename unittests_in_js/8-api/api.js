f1c9d9fe4cc5678178ee1ef9ebca1ae505741449
root <jdarahthomas@gmail.com>
2023-10-26 21:15:22 +0000

    task 8

--- unittests_in_js/8-api/api.js
+++ unittests_in_js/8-api/api.js
@@ -1,14 +1,14 @@
 const express = require('express');
 
 const app = express();
-const connect = 7885;
+const port = 7885;
 
 app.get('/', (req, res) => {
     res.send('Welcome to the payment system');
 });
 
-app.listen(connect, () => {
-    console.log('API available on localhost port ${connect}');
+app.listen(port, () => {
+    console.log(`API available on localhost port ${port}`);
 });
 
 module.exports = app;
