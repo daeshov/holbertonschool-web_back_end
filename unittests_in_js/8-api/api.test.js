bf461a304c405ec8a14cf5dc27d32bb784df5abe
root <jdarahthomas@gmail.com>
2023-10-26 21:34:52 +0000

    task 8

--- unittests_in_js/8-api/api.test.js
+++ unittests_in_js/8-api/api.test.js
@@ -1,31 +1,14 @@
-const chai = require('chai');
-const chaiHttp = require('chai-http');
-const app = require('./your-express-app'); // Replace with the actual path to your Express app
-const expect = chai.expect;
+const request = require('request');
+const chai = require('chai')
+const { expect } = require('chai');
 
-chai.use(chaiHttp);
-
-describe('API Index Page', () => {
-  it('should return the correct status code', (done) => {
-    chai
-      .request(app)
-      .get('/')
-      .end((err, res) => {
-        expect(res).to.have.status(200); // You can adjust the status code as needed
-        done();
-      });
+describe('API test', () => {
+  it('respond with 200 code and body', (done) => {
+    request('http://localhost:7865', 'GET', (error, res, body) => {
+      if (error) throw error;
+      expect(res.statusCode).to.equal(200);
+      expect(body).to.equal('Welcome to the payment system');
+    });
+    done();
   });
-
-  it('should return the correct result', (done) => {
-    chai
-      .request(app)
-      .get('/')
-      .end((err, res) => {
-        expect(res.text).to.equal('Welcome to the payment system'); // You can adjust the expected result
-        done();
-      });
-  });
-
-  // You can add additional tests as needed
-  // For example, you might test the response headers, content type, or other aspects of the API.
 });
