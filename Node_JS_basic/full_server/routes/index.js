32bb6379bdbcc5789d52637ad92a11a6b40bcb62
root <jdarahthomas@gmail.com>
2023-10-31 21:02:01 +0000

    task 8

--- Node_JS_basic/full_server/routes/index.js
+++ Node_JS_basic/full_server/routes/index.js
@@ -4,8 +4,8 @@ const StudentsController = require('../controllers/StudentsController');
 
 const router = express.Router();
 
-router.get('/', (req, res) => AppController.getHomepage(req, res));
-router.get('/students', (req, res) => StudentsController.getAllStudents(req, res));
-router.get('/students/:major', (req, res) => StudentsController.getAllStudentsByMajor(req, res));
+router.get('/', AppController.getHomepage);
+router.get('/students', StudentsController.getAllStudents);
+router.get('/students/:major', StudentsController.getAllStudentsByMajor);
 
 module.exports = router;
